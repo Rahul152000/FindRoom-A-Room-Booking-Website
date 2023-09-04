36acb96a808933ffbdd5b05740b78152d746f1d5
Rahul152000 <142194455+Rahul152000@users.noreply.github.com>
2023-09-04 17:18:20 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,8 +0,0 @@
-var mysql =require("mysql");
-var con= mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"rahul@a143",
-    database:"Register"
-});
-module.exports=con;
